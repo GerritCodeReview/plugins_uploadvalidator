cbaa92a591d9420a84c7e929bbf1b08700a515bc
Christian Aistleitner <christian@quelltextlich.at>
2015-06-06 18:54:26 +0200

    Close Git and TreeWalk resource leaks
    The Git and TreeWalk never got closed, so they leaked resources. We
    convert their use to AutoCloseable.
    
    Change-Id: I1a547dba860b6841ce0b569303956fce7df6dd44

--- src/main/java/com/googlesource/gerrit/plugins/uploadvalidator/PathValidator.java
+++ src/main/java/com/googlesource/gerrit/plugins/uploadvalidator/PathValidator.java
@@ -38,21 +38,24 @@ public abstract class PathValidator implements CommitValidationListener {
     List<String> files = new ArrayList<>();
 
     if (c.getParentCount() > 0) {
-      Git git = new Git(repo);
-      List<DiffEntry> diffEntries =
-          git.diff().setOldTree(getTreeIterator(repo, c.getName() + "^"))
-              .setNewTree(getTreeIterator(repo, c.getName())).call();
+      List<DiffEntry> diffEntries;
+      try (final Git git = new Git(repo)) {
+        diffEntries = git.diff()
+            .setOldTree(getTreeIterator(repo, c.getName() + "^"))
+            .setNewTree(getTreeIterator(repo, c.getName())).call();
+      }
       for (DiffEntry e : diffEntries) {
         if (e.getNewPath() != null) {
           files.add(e.getNewPath());
         }
       }
     } else {
-      TreeWalk tw = new TreeWalk(repo);
-      tw.addTree(c.getTree());
-      tw.setRecursive(true);
-      while (tw.next()) {
-        files.add(tw.getPathString());
+      try (final TreeWalk tw = new TreeWalk(repo)) {
+        tw.addTree(c.getTree());
+        tw.setRecursive(true);
+        while (tw.next()) {
+          files.add(tw.getPathString());
+        }
       }
     }
 
