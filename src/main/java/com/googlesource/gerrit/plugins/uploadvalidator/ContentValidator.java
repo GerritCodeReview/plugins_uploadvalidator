0314afa4544b4fdc952877ee4c74e14b8ee81c11
Pascal Krause <pascal.krause@sap.com>
2016-02-19 13:20:32 +0100

    Add validator that checks for Windows line endings
    Project owner can now configure invalid line endings. Commits with
    files which contain carriage return (CR) characters will be rejected.
    
    Change-Id: Iea2a1c0ce26aa9b8ad27fae3b78dd38158a614fc

---
+++ src/main/java/com/googlesource/gerrit/plugins/uploadvalidator/ContentValidator.java
@@ -0,0 +1,73 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.uploadvalidator;
+
+import com.google.gerrit.server.git.validators.CommitValidationListener;
+
+import org.eclipse.jgit.lib.FileMode;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.jgit.treewalk.filter.TreeFilter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class ContentValidator implements CommitValidationListener {
+
+  protected Map<ObjectId, String> getContent(Repository repo, RevCommit c)
+      throws IOException {
+    Map<ObjectId, String> content = new HashMap<>();
+
+    try (TreeWalk tw = new TreeWalk(repo)) {
+      tw.setRecursive(true);
+      tw.setFilter(TreeFilter.ANY_DIFF);
+      tw.addTree(c.getTree());
+      if (c.getParentCount() > 0) {
+        for (RevCommit p : c.getParents()) {
+          tw.addTree(p.getTree());
+        }
+        while (tw.next()) {
+          boolean diff = true;
+          if (c.getParentCount() > 1) {
+            for (int p = 1; p <= c.getParentCount(); p++) {
+              if (tw.getObjectId(0).equals(tw.getObjectId(p))) {
+                diff = false;
+                break;
+              }
+            }
+          }
+          if (diff) {
+            addContent(content, tw);
+          }
+        }
+      } else {
+        while(tw.next()) {
+          addContent(content, tw);
+        }
+      }
+    }
+    return content;
+  }
+
+  private static void addContent(Map<ObjectId, String> content, TreeWalk tw) {
+    if (FileMode.EXECUTABLE_FILE.equals(tw.getRawMode(0))
+        || FileMode.REGULAR_FILE.equals(tw.getRawMode(0))) {
+      content.put(tw.getObjectId(0), tw.getPathString());
+    }
+  }
+}
