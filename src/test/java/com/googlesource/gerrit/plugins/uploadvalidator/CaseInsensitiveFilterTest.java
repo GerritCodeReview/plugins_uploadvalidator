a00bf2986db00fbad3861b375b0e30118b0ac997
Pascal Krause <pascal.krause@sap.com>
2016-02-23 09:40:58 +0100

    Add validator that checks for duplicate pathnames
    Project owner can now configure duplicate pathnames. Commits which
    contain duplicate pathnames will be rejected. If 'y' and 'z' are
    pathnames and y.equalsIgnoreCase(z), then 'y' and 'z' are duplicates.
    
    Change-Id: Iaac644a07fc88e855cc9a041f0237157bda9d010

---
+++ src/test/java/com/googlesource/gerrit/plugins/uploadvalidator/CaseInsensitiveFilterTest.java
@@ -0,0 +1,97 @@
+package com.googlesource.gerrit.plugins.uploadvalidator;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.googlesource.gerrit.plugins.uploadvalidator.TestUtils.createEmptyDirCacheEntries;
+import static com.googlesource.gerrit.plugins.uploadvalidator.TestUtils.makeCommit;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Sets;
+
+import org.eclipse.jgit.junit.TestRepository;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+
+public class CaseInsensitiveFilterTest extends ValidatorTestCase {
+  private TestRepository<Repository> testRepo;
+
+  @Override
+  @Before
+  public void init() throws IOException {
+    super.init();
+    testRepo = new TestRepository<>(repo);
+  }
+
+  private List<String> getVisitedTreePaths(List<String> existingTreePaths,
+      String testPath) throws Exception {
+    return getVisitedTreePaths(existingTreePaths, Sets.newHashSet(testPath));
+  }
+
+  private List<String> getVisitedTreePaths(List<String> existingTreePaths,
+      Set<String> testPaths) throws Exception {
+    List<String> found = new ArrayList<>();
+    RevCommit c = makeCommit(
+        createEmptyDirCacheEntries(existingTreePaths, testRepo), testRepo);
+    try (TreeWalk tw = new TreeWalk(repo)) {
+      CaseInsensitiveFilter f =
+          new CaseInsensitiveFilter(testPaths, Locale.ENGLISH);
+      tw.setFilter(f);
+      tw.setRecursive(f.shouldBeRecursive());
+      tw.addTree(c.getTree());
+      while (tw.next()) {
+        if (tw.isSubtree()) {
+          tw.enterSubtree();
+        }
+        found.add(tw.getPathString());
+      }
+    }
+    return found;
+  }
+
+  @Test
+  public void testVisitOnlyPathsWithMatchingPrefix() throws Exception {
+    List<String> INITIAL_PATHNAMES = ImmutableList.of(
+        "a", "b",
+        "folder1/a", "folder1/A",
+        "Folder1/A", "Folder1/B",
+        "Folder2/a");
+
+    String testPath = "folder1/a";
+
+    List<String> expected = ImmutableList.of(
+        "folder1", "folder1/a", "folder1/A",
+        "Folder1", "Folder1/A");
+
+    List<String> result =
+        getVisitedTreePaths(INITIAL_PATHNAMES, testPath);
+    assertThat(result).containsExactlyElementsIn(expected);
+  }
+
+  @Test
+  public void testVisitOnlyPathsWithMatchingPrefixAndDepth() throws Exception {
+    List<String> INITIAL_PATHNAMES = ImmutableList.of(
+        "folder1/a",
+        "folder1/A",
+        "Folder1/a/a");
+
+    String testPath = "folder1/a";
+
+    // It's important that "Folder1/a" is not included in results.
+    // Folder1/a matches the prefix, but has a different depth.
+    List<String> expected = ImmutableList.of(
+        "folder1", "folder1/a", "folder1/A",
+        "Folder1");
+
+    List<String> result =
+        getVisitedTreePaths(INITIAL_PATHNAMES, testPath);
+    assertThat(result).containsExactlyElementsIn(expected);
+  }
+}
