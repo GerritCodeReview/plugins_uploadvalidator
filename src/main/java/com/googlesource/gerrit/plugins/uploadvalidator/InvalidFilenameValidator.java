c6a797ff17ae181e1a2a32ad090f4f5896aa4fc1
Pascal Krause <pascal.krause@sap.com>
2016-02-26 17:39:11 +0100

    Add unit test for InvalidFilenameValidator
    With the old structure of the validator it was very hard to write a unit
    test. Because of that the structure of InvalidFileNameValidator has
    changed.
    
    Change-Id: I8131a72efd67966a5c2abb9df04995e1209069fa

--- src/main/java/com/googlesource/gerrit/plugins/uploadvalidator/InvalidFilenameValidator.java
+++ src/main/java/com/googlesource/gerrit/plugins/uploadvalidator/InvalidFilenameValidator.java
@@ -26,6 +26,7 @@ import com.google.gerrit.server.project.NoSuchProjectException;
 import com.google.inject.Inject;
 
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -34,7 +35,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Pattern;
 
-public class InvalidFilenameValidator implements CommitValidationListener {
+public class InvalidFilenameValidator
+    implements CommitValidationListener, Validator {
   public static String KEY_INVALID_FILENAME_PATTERN = "invalidFilenamePattern";
 
   private final String pluginName;
@@ -56,28 +58,16 @@ public class InvalidFilenameValidator implements CommitValidationListener {
       PluginConfig cfg =
           cfgFactory.getFromProjectConfig(
               receiveEvent.project.getNameKey(), pluginName);
-      List<Pattern> invalidFilenamePatterns = new ArrayList<>();
-      for (String s : cfg.getStringList(KEY_INVALID_FILENAME_PATTERN)) {
-        invalidFilenamePatterns.add(Pattern.compile(s));
+      if (cfg.getStringList(KEY_INVALID_FILENAME_PATTERN).length == 0) {
+        return Collections.emptyList();
       }
-      if (invalidFilenamePatterns.size() > 0) {
-        try (Repository repo = repoManager.openRepository(
-            receiveEvent.project.getNameKey())) {
-          List<CommitValidationMessage> messages = new LinkedList<>();
-          for (String file : CommitUtils.getChangedPaths(
-              repo, receiveEvent.commit)) {
-            for (Pattern p : invalidFilenamePatterns) {
-              if (p.matcher(file).find()) {
-                messages.add(new CommitValidationMessage(
-                    "invalid characters found in filename: " + file, true));
-                break;
-              }
-            }
-          }
-          if (!messages.isEmpty()) {
-            throw new CommitValidationException(
-                "contains files with an invalid filename", messages);
-          }
+      try (Repository repo = repoManager.openRepository(
+          receiveEvent.project.getNameKey())) {
+        List<CommitValidationMessage> messages = performValidation(
+            repo, receiveEvent.commit, cfg);
+        if (!messages.isEmpty()) {
+          throw new CommitValidationException(
+              "contains files with an invalid filename", messages);
         }
       }
     } catch (NoSuchProjectException | IOException e) {
@@ -85,5 +75,25 @@ public class InvalidFilenameValidator implements CommitValidationListener {
           "failed to check on invalid file names", e);
     }
     return Collections.emptyList();
+  }
+
+  @Override
+  public List<CommitValidationMessage> performValidation(Repository repo,
+      RevCommit c, PluginConfig cfg) throws IOException {
+    List<Pattern> invalidFilenamePatterns = new ArrayList<>();
+    for (String s : cfg.getStringList(KEY_INVALID_FILENAME_PATTERN)) {
+      invalidFilenamePatterns.add(Pattern.compile(s));
+    }
+    List<CommitValidationMessage> messages = new LinkedList<>();
+    for (String file : CommitUtils.getChangedPaths(repo, c)) {
+      for (Pattern p : invalidFilenamePatterns) {
+        if (p.matcher(file).find()) {
+          messages.add(new CommitValidationMessage(
+              "invalid characters found in filename: " + file, true));
+          break;
+        }
+      }
+    }
+    return messages;
   }
 }
---
+++ src/test/java/com/googlesource/gerrit/plugins/uploadvalidator/TestUtils.java
@@ -0,0 +1,93 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.uploadvalidator;
+
+import com.google.gerrit.server.git.validators.CommitValidationMessage;
+
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jgit.api.AddCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.NoFilepatternException;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+public class TestUtils {
+  public static Repository createNewRepository(File repoFolder)
+      throws IOException {
+    Repository repository =
+        FileRepositoryBuilder.create(new File(repoFolder, ".git"));
+    repository.create();
+    return repository;
+  }
+
+  public static RevCommit makeCommit(Repository repo, String message,
+      Set<File> files)
+          throws IOException, NoFilepatternException, GitAPIException {
+    try (Git git = new Git(repo)) {
+      if (files != null) {
+        addFiles(git, files);
+      }
+      return git.commit().setMessage(message).call();
+    }
+  }
+
+  private static void addFiles(Git git, Set<File> files)
+      throws IOException, NoFilepatternException, GitAPIException {
+    AddCommand ac = git.add();
+    for (File f : files) {
+      if (!f.exists()) {
+        FileUtils.touch(f);
+      }
+      String p = f.getAbsolutePath()
+          .replace(git.getRepository().getWorkTree().getAbsolutePath(), "")
+          .substring(1);
+      ac = ac.addFilepattern(p);
+    }
+    ac.call();
+  }
+
+  public static boolean compareCommitValidationMessage(
+      List<CommitValidationMessage> m1, List<CommitValidationMessage> m2) {
+    for (CommitValidationMessage cvm1 : m1) {
+      boolean found = false;
+      for (CommitValidationMessage cvm2 : m2) {
+        if (compareCommitValidationMessage(cvm1, cvm2)) {
+          found = true;
+        }
+      }
+      if (!found) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static boolean compareCommitValidationMessage(
+      CommitValidationMessage msg1, CommitValidationMessage msg2) {
+    if (msg1.getMessage().equals(msg2.getMessage())
+        && msg1.isError() == msg2.isError()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
