9090a842af6445d1fca931dbda24043e1107181f
Pascal Krause <pascal.krause@sap.com>
2016-03-01 11:48:24 +0100

    Move functionality of PathValidator into util class
    Every validator that wants to know which files changed in the current
    push was forced to extend PathValidator.java. With the extraction of
    this functionality into ChangesUtils.java, a validator don't have to
    extend this class.
    
    Also added a method to get the ObjectIds of files that have changed.
    
    Change-Id: I84975741725244603ae7150da89c0f985fbbd398

--- src/main/java/com/googlesource/gerrit/plugins/uploadvalidator/MaxPathLengthValidator.java
+++ src/main/java/com/googlesource/gerrit/plugins/uploadvalidator/MaxPathLengthValidator.java
@@ -20,19 +20,20 @@ import com.google.gerrit.server.config.PluginConfigFactory;
 import com.google.gerrit.server.events.CommitReceivedEvent;
 import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.gerrit.server.git.validators.CommitValidationException;
+import com.google.gerrit.server.git.validators.CommitValidationListener;
 import com.google.gerrit.server.git.validators.CommitValidationMessage;
 import com.google.gerrit.server.project.NoSuchProjectException;
 import com.google.inject.Inject;
 
-import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Repository;
 
 import java.io.IOException;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
-public class MaxPathLengthValidator extends PathValidator {
+public class MaxPathLengthValidator implements CommitValidationListener {
   public static String KEY_MAX_PATH_LENGTH = "maxPathLength";
 
   private final String pluginName;
@@ -58,7 +59,8 @@ public class MaxPathLengthValidator extends PathValidator {
       if (maxPathLength > 0) {
         try (Repository repo = repoManager.openRepository(receiveEvent.project.getNameKey())) {
           List<CommitValidationMessage> messages = new LinkedList<>();
-          List<String> files = getFiles(repo, receiveEvent.commit);
+          Set<String> files = ChangeUtils.getChangedPaths(
+              repo, receiveEvent.commit);
           for (String file : files) {
             if (file.length() > maxPathLength) {
               messages.add(new CommitValidationMessage("path too long: " + file, true));
@@ -71,7 +73,7 @@ public class MaxPathLengthValidator extends PathValidator {
           }
         }
       }
-    } catch (NoSuchProjectException | IOException | GitAPIException e) {
+    } catch (NoSuchProjectException | IOException e) {
       throw new CommitValidationException("failed to check for max file path length", e);
     }
 
