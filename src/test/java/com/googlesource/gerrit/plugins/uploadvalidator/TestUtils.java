52ed8ba476ad412458c90cc917d3c5e08572c9b9
Pascal Krause <pascal.krause@sap.com>
2016-03-03 09:02:25 +0100

    Add unit test for MaxPathLengthValidator
    With the old structure of the validator it was very hard to write a unit
    test. Because of that the structure of MaxPathLengthValidator has
    changed.
    
    Change-Id: I03b854f328e4e65672b092642efae1064fb13412

--- src/test/java/com/googlesource/gerrit/plugins/uploadvalidator/TestUtils.java
+++ src/test/java/com/googlesource/gerrit/plugins/uploadvalidator/TestUtils.java
@@ -22,6 +22,7 @@ import com.google.gerrit.server.git.validators.CommitValidationMessage;
 import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.AddCommand;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.RmCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.NoFilepatternException;
 import org.eclipse.jgit.lib.Config;
@@ -69,6 +70,21 @@ public class TestUtils {
     }
   }
 
+  private static String generateFilePattern(File f, Git git) {
+    return f.getAbsolutePath()
+        .replace(git.getRepository().getWorkTree().getAbsolutePath(), "")
+        .substring(1);
+  }
+
+  public static void removeFiles(Git git, Set<File> files)
+      throws NoFilepatternException, GitAPIException {
+    RmCommand rmc = git.rm();
+    for (File f : files) {
+      rmc.addFilepattern(generateFilePattern(f, git));
+    }
+    rmc.call();
+  }
+
   private static void addFiles(Git git, Map<File, byte[]> files)
       throws IOException, NoFilepatternException, GitAPIException {
     AddCommand ac = git.add();
@@ -79,12 +95,13 @@ public class TestUtils {
       if (files.get(f) != null) {
         FileUtils.writeByteArrayToFile(f, files.get(f));
       }
-      String p = f.getAbsolutePath()
-          .replace(git.getRepository().getWorkTree().getAbsolutePath(), "")
-          .substring(1);
-      ac = ac.addFilepattern(p);
+      ac = ac.addFilepattern(generateFilePattern(f, git));
     }
     ac.call();
+  }
+
+  public static File createEmptyFile(String name, Repository repo) {
+    return new File(repo.getDirectory().getParent(), name);
   }
 
   public static List<String> transformMessages(
