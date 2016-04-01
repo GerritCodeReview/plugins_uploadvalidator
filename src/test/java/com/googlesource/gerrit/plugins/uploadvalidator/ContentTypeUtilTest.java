8ee8cd41a573f1242310359a08ffc1f750cadebc
Pascal Krause <pascal.krause@sap.com>
2016-04-01 14:31:14 +0200

    Add ContentTypeUtil to detect binary files
    Some checks shouldn't run on binary files (e.g.  InvalidLineEndingCheck).
    The InvalidLineEndingCheck already had a config parameter to define which
    files are binary. But the BlockedKeywordCheck also needs to know which
    files are binary. Because of this the ContentTypeUtil is introduced.
    
    The config parameter ignoreFilesWhenCheckLineEndings is removed and a
    new parameter binaryType is introduced to configure which content types
    are considered binary.
    
    To detect content type, we use Apache Tika [1]. For using Apache Tika a
    new dependency [2] is needed.
    
    It is not possible to use the already provided MimeUtil2 library from
    Gerrit's plugin-api because MimeUtil2 [3] wrongly detects, for example,
    the content type of project.config as application/octet-stream. Apache
    Tika properly detects content type of the project.config as text/plain.
    Also, it seems that MimeUtil2 is not further developed.
    
    [1] https://tika.apache.org/
    [2] org.apache.tika:tika-core:1.12
    [3] http://mime-util.sourceforge.net/project-info.html
    
    Change-Id: Ia2529f187b8362ad2f21bb5e9bd9d0f88eb775b5

---
+++ src/test/java/com/googlesource/gerrit/plugins/uploadvalidator/ContentTypeUtilTest.java
@@ -0,0 +1,65 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.uploadvalidator;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.googlesource.gerrit.plugins.uploadvalidator.TestUtils.EMPTY_PLUGIN_CONFIG;
+import static com.googlesource.gerrit.plugins.uploadvalidator.TestUtils.PATTERN_CACHE;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+
+public class ContentTypeUtilTest {
+
+  private ContentTypeUtil ctu;
+
+  @Before
+  public void setUp() {
+    ctu = new ContentTypeUtil(PATTERN_CACHE);
+  }
+
+  @Test
+  public void testMatchesAny() throws ExecutionException {
+    String[] patterns =
+        new String[] {"text/*", "^application/(pdf|xml)", "application/zip"};
+
+    matchesAny("text/xml", patterns);
+    matchesAny("text/html", patterns);
+    matchesAny("text/plain", patterns);
+    matchesAny("application/pdf", patterns);
+    matchesAny("application/xml", patterns);
+    matchesAny("application/zip", patterns);
+
+    noMatch("foo/bar", patterns);
+    noMatch("application/msword", patterns);
+  }
+
+  private void matchesAny(String p, String[] patterns)
+      throws ExecutionException {
+    assertThat(ctu.matchesAny(p, patterns)).isTrue();
+  }
+
+  private void noMatch(String p, String[] patterns)
+      throws ExecutionException {
+    assertThat(ctu.matchesAny(p, patterns)).isFalse();
+  }
+
+  @Test
+  public void noBinaryTypesWhenConfigEmpty() {
+    assertThat(ContentTypeUtil.getBinaryTypes(EMPTY_PLUGIN_CONFIG)).isEmpty();
+  }
+}
