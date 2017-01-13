6706eac62406d49de0b9c8f85038785eb638fa2a
Luca Milanesio <luca.milanesio@gmail.com>
2017-01-13 00:03:58 +0000

    Allows exception to the rules
    At times people need to be able to avoid the upload validation
    rules to actually manage the repository structure or perform
    specific operations.
    
    E.g. A release manager should be able to upload some generated
    artifacts and thus override the blockage of upload binary files
    that is imposed to all other users.
    
    By defining exception to the rules, it is possible to allow
    individual users or groups to skip the rules for projects or branches.
    
    Change-Id: I1e11288cd60690754ff4a38499dfcc4385190125

--- src/main/java/com/googlesource/gerrit/plugins/uploadvalidator/ValidatorConfig.java
+++ src/main/java/com/googlesource/gerrit/plugins/uploadvalidator/ValidatorConfig.java
@@ -15,44 +15,40 @@
 package com.googlesource.gerrit.plugins.uploadvalidator;
 
 import com.google.gerrit.common.data.RefConfigSection;
-import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gerrit.reviewdb.client.AccountGroup;
 import com.google.gerrit.reviewdb.client.Project;
-import com.google.gerrit.server.config.PluginConfigFactory;
-import com.google.gerrit.server.project.ProjectCache;
-import com.google.gerrit.server.project.ProjectState;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.account.GroupCache;
+import com.google.gerrit.server.config.PluginConfig;
 import com.google.gerrit.server.project.RefPatternMatcher;
 import com.google.inject.Inject;
+import com.google.inject.Provider;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.Arrays;
+import java.util.Optional;
 
 public class ValidatorConfig {
-  private static final Logger log = LoggerFactory.getLogger(ValidatorConfig.class);
-
-  private final String pluginName;
-  private final ProjectCache projectCache;
-  private final PluginConfigFactory pluginCfgFactory;
+  private final ConfigFactory pluginCfgFactory;
+  private final Provider<CurrentUser> userProvider;
+  private final GroupCache groupCache;
 
   @Inject
-  public ValidatorConfig(@PluginName String pluginName,
-      ProjectCache projectCache,
-      PluginConfigFactory pluginCfgFactory) {
-    this.pluginName = pluginName;
-    this.projectCache = projectCache;
+  public ValidatorConfig(ConfigFactory pluginCfgFactory,
+      Provider<CurrentUser> userProvider, GroupCache groupCache) {
     this.pluginCfgFactory = pluginCfgFactory;
+    this.userProvider = userProvider;
+    this.groupCache = groupCache;
   }
 
-  public boolean isEnabledForRef(Project.NameKey projectName, String refName) {
-    ProjectState project = projectCache.get(projectName);
-    if (project == null) {
-      log.error("Failed to check if " + pluginName + " is enabled for project "
-          + projectName.get() + ": Project " + projectName.get() + " not found");
+  public boolean isEnabledForRef(Project.NameKey projectName, String refName,
+      String validatorOp) {
+    PluginConfig pluginProjectConfig = pluginCfgFactory.get(projectName);
+    if (pluginProjectConfig == null
+        || canSkipOnRef(pluginProjectConfig, refName, validatorOp)) {
       return false;
     }
 
-    String[] refPatterns =
-        pluginCfgFactory.getFromProjectConfigWithInheritance(project,
-            pluginName).getStringList("branch");
+    String[] refPatterns = pluginProjectConfig.getStringList("branch");
     if (refPatterns.length == 0) {
       return true; // Default behavior: no branch-specific config
     }
@@ -63,6 +59,61 @@ public class ValidatorConfig {
       }
     }
     return false;
+  }
+
+  private boolean canSkipOnRef(PluginConfig conf, String refName,
+      String validation) {
+    String[] skipGroups = conf.getStringList("skipGroup");
+    Optional<Boolean> skipOnValidation =
+        skipValidationBasedOn(conf, "skipValidation", validation);
+    Optional<Boolean> skipOnBranch =
+        skipValidationBasedOn(conf, "skipBranch", refName);
+    if (skipGroups.length == 0
+        || (skipOnValidation.isPresent() && !skipOnValidation.get())
+        || (skipOnBranch.isPresent() && !skipOnBranch.get())) {
+      return false;
+    }
+
+    return canSkipOnGroup(skipGroups);
+  }
+
+  private Optional<Boolean> skipValidationBasedOn(
+      PluginConfig pluginProjectConfig, String criteria, String value) {
+    String[] criteriaValues = pluginProjectConfig.getStringList(criteria);
+    if (criteriaValues.length == 0) {
+      return Optional.empty();
+    }
+
+    for (String s : criteriaValues) {
+      if (s.equals(value)) {
+        return Optional.of(true);
+      }
+    }
+    return Optional.of(false);
+  }
+
+  private boolean canSkipOnGroup(String[] skipGroups) {
+    if (skipGroups.length == 0) {
+      return false;
+    }
+
+    CurrentUser user = userProvider.get();
+    if (!user.isIdentifiedUser()) {
+      return false;
+    }
+
+    return user
+        .asIdentifiedUser()
+        .getEffectiveGroups()
+        .containsAnyOf(Arrays.stream(skipGroups).map(this::groupUUID)::iterator);
+  }
+
+  private AccountGroup.UUID groupUUID(String groupNameOrUUID) {
+    Optional<AccountGroup.UUID> uuidFromCache =
+        Optional.ofNullable(
+            groupCache.get(new AccountGroup.NameKey(groupNameOrUUID))).map(
+            group -> group.getGroupUUID());
+    return uuidFromCache.orElse(new AccountGroup.UUID(groupNameOrUUID));
   }
 
   private static boolean match(String refName, String refPattern) {
---
+++ src/test/java/com/googlesource/gerrit/plugins/uploadvalidator/FakeUserProvider.java
@@ -0,0 +1,44 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.uploadvalidator;
+
+import static org.easymock.EasyMock.*;
+
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.inject.Provider;
+
+public class FakeUserProvider implements Provider<CurrentUser> {
+  private final String[] groupUUID;
+
+  public FakeUserProvider(String... groupUUID) {
+    this.groupUUID = groupUUID;
+  }
+
+  @Override
+  public CurrentUser get() {
+    return createNew();
+  }
+
+  private IdentifiedUser createNew() {
+    IdentifiedUser user = createMock(IdentifiedUser.class);
+    expect(user.isIdentifiedUser()).andReturn(true);
+    expect(user.asIdentifiedUser()).andReturn(user);
+    expect(user.getEffectiveGroups()).andReturn(
+        new FakeGroupMembership(groupUUID));
+    replay(user);
+    return user;
+  }
+}
