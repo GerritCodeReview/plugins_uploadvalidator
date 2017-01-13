5ab69be0c41912ba43baf191a5a8dab1bfbf4638
Luca Milanesio <luca.milanesio@gmail.com>
2017-01-13 00:03:58 +0000

    Allows exception to the rules
    At times people need to be able to avoid the upload validation
    rules to actually manage the repository structure or perform
    specific operations.
    
    E.g. A release manager should be able to upload some generated
    artifacts and thus override the blockage of upload binary files
    that is imposed to all other users.
    
    By defining exception to the rules, it is possible to allow
    individual users or groups to skip the rules for projects or branches.
    
    Change-Id: I1e11288cd60690754ff4a38499dfcc4385190125

---
+++ src/test/java/com/googlesource/gerrit/plugins/uploadvalidator/FakeConfigFactory.java
@@ -0,0 +1,43 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.uploadvalidator;
+
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.reviewdb.client.Project.NameKey;
+import com.google.gerrit.server.config.PluginConfig;
+
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.Config;
+
+public class FakeConfigFactory implements ConfigFactory {
+  private final Config config;
+  private final Project.NameKey projectName;
+
+  public FakeConfigFactory(Project.NameKey projectName, String configText)
+      throws ConfigInvalidException {
+    this.config = new Config();
+    this.config.fromText(configText);
+    this.projectName = projectName;
+  }
+
+  @Override
+  public PluginConfig get(NameKey projectName) {
+    if (this.projectName.equals(projectName)) {
+      return new PluginConfig("uploadvalidator", config);
+    }
+
+    return new PluginConfig("uploadvalidator", new Config());
+  }
+}
---
+++ src/test/java/com/googlesource/gerrit/plugins/uploadvalidator/SkipValidationTest.java
@@ -0,0 +1,129 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.uploadvalidator;
+
+import static com.google.common.truth.Truth.assertThat;
+import com.google.gerrit.reviewdb.client.AccountGroup;
+
+import com.google.gerrit.reviewdb.client.Project;
+
+import org.junit.Test;
+
+public class SkipValidationTest {
+  private Project.NameKey projectName = new Project.NameKey("testProject");
+
+  @Test
+  public void dontSkipByDefault() throws Exception {
+    ValidatorConfig validatorConfig =
+        new ValidatorConfig(new FakeConfigFactory(projectName, ""),
+            new FakeUserProvider(), new FakeGroupCacheUUIDByName());
+
+    assertThat(validatorConfig.isEnabledForRef(projectName, "anyRef", "anyOp"))
+        .isTrue();
+  }
+
+  @Test
+  public void skipWhenUserBelongsToGroupUUID() throws Exception {
+    String config =
+        "[plugin \"uploadvalidator\"]\n" + "skipValidation=testOp\n"
+            + "skipGroup=testGroup\n" + "skipGroup=anotherGroup";
+
+    ValidatorConfig validatorConfig =
+        new ValidatorConfig(new FakeConfigFactory(projectName, config),
+            new FakeUserProvider("testGroup", "yetAnotherGroup"),
+            new FakeGroupCacheUUIDByName());
+
+    assertThat(validatorConfig.isEnabledForRef(projectName, "anyRef", "testOp"))
+        .isFalse();
+  }
+
+  @Test
+  public void skipWhenUserBelongsToGroupName() throws Exception {
+    String config =
+        "[plugin \"uploadvalidator\"]\n" + "skipValidation=testOp\n"
+            + "skipGroup=testGroupName\n";
+
+    ValidatorConfig validatorConfig =
+        new ValidatorConfig(new FakeConfigFactory(projectName, config),
+            new FakeUserProvider("testGroupId"), new FakeGroupCacheUUIDByName(
+                new AccountGroup(new AccountGroup.NameKey("testGroupName"),
+                    new AccountGroup.Id(1),
+                    new AccountGroup.UUID("testGroupId"))));
+
+    assertThat(validatorConfig.isEnabledForRef(projectName, "anyRef", "testOp"))
+        .isFalse();
+  }
+
+  @Test
+  public void dontSkipWhenUserBelongsToOtherGroupsUUID() throws Exception {
+    String config =
+        "[plugin \"uploadvalidator\"]\n" + "skipValidation=testOp\n"
+            + "skipGroup=testGroup\n" + "skipGroup=anotherGroup";
+
+    ValidatorConfig validatorConfig =
+        new ValidatorConfig(new FakeConfigFactory(projectName, config),
+            new FakeUserProvider("yetAnotherGroup"),
+            new FakeGroupCacheUUIDByName());
+
+    assertThat(validatorConfig.isEnabledForRef(projectName, "anyRef", "testOp"))
+        .isTrue();
+  }
+
+  @Test
+  public void dontSkipForOtherOps() throws Exception {
+    String config =
+        "[plugin \"uploadvalidator\"]\n" + "skipValidation=testOp\n"
+            + "skipGroup=testGroup\n" + "skipGroup=anotherGroup";
+
+    ValidatorConfig validatorConfig =
+        new ValidatorConfig(new FakeConfigFactory(projectName, config),
+            new FakeUserProvider("testGroup", "yetAnotherGroup"),
+            new FakeGroupCacheUUIDByName());
+
+    assertThat(
+        validatorConfig.isEnabledForRef(projectName, "anyRef", "anotherOp"))
+        .isTrue();
+  }
+
+  @Test
+  public void skipOnlyOnSpecificRef() throws Exception {
+    String config =
+        "[plugin \"uploadvalidator\"]\n" + "skipValidation=testOp\n"
+            + "skipBranch=refs/heads/myref\n" + "skipGroup=testGroup";
+
+    ValidatorConfig validatorConfig =
+        new ValidatorConfig(new FakeConfigFactory(projectName, config),
+            new FakeUserProvider("testGroup"), new FakeGroupCacheUUIDByName());
+
+    assertThat(
+        validatorConfig.isEnabledForRef(projectName, "refs/heads/myref",
+            "testOp")).isFalse();
+  }
+
+  @Test
+  public void dontSkipOnOtherRefs() throws Exception {
+    String config =
+        "[plugin \"uploadvalidator\"]\n" + "skipValidation=testOp\n"
+            + "skipBranch=refs/heads/myref\n" + "skipGroup=testGroup";
+
+    ValidatorConfig validatorConfig =
+        new ValidatorConfig(new FakeConfigFactory(projectName, config),
+            new FakeUserProvider("testGroup"), new FakeGroupCacheUUIDByName());
+
+    assertThat(
+        validatorConfig.isEnabledForRef(projectName, "refs/heads/anotherRef",
+            "testOp")).isTrue();
+  }
+}
