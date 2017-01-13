859736d1950be9e3a7c668f7c1aae553c11bdc59
Luca Milanesio <luca.milanesio@gmail.com>
2017-01-13 00:03:58 +0000

    Allows exception to the rules
    At times people need to be able to avoid the upload validation
    rules to actually manage the repository structure or perform
    specific operations.
    
    E.g. A release manager should be able to upload some generated
    artifacts and thus override the blockage of upload binary files
    that is imposed to all other users.
    
    By defining exception to the rules, it is possible to allow
    individual users or groups to skip the rules for projects or branches.
    
    Change-Id: I1e11288cd60690754ff4a38499dfcc4385190125

--- src/main/java/com/googlesource/gerrit/plugins/uploadvalidator/SymlinkValidator.java
+++ src/main/java/com/googlesource/gerrit/plugins/uploadvalidator/SymlinkValidator.java
@@ -87,8 +87,9 @@ public class SymlinkValidator implements CommitValidationListener {
       PluginConfig cfg =
           cfgFactory.getFromProjectConfigWithInheritance(
               receiveEvent.project.getNameKey(), pluginName);
-      if (isActive(cfg) && validatorConfig.isEnabledForRef(
-          receiveEvent.getProjectNameKey(), receiveEvent.getRefName())) {
+      if (isActive(cfg)
+          && validatorConfig.isEnabledForRef(receiveEvent.getProjectNameKey(),
+              receiveEvent.getRefName(), KEY_CHECK_SYMLINK)) {
         try (Repository repo =
             repoManager.openRepository(receiveEvent.project.getNameKey())) {
           List<CommitValidationMessage> messages =
