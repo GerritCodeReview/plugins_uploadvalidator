24b2039e0dea78801470a170d4435550c9d0ff05
Luca Milanesio <luca.milanesio@gmail.com>
2017-01-13 00:03:58 +0000

    Allows exception to the rules
    At times people need to be able to avoid the upload validation
    rules to actually manage the repository structure or perform
    specific operations.
    
    E.g. A release manager should be able to upload some generated
    artifacts and thus override the blockage of upload binary files
    that is imposed to all other users.
    
    By defining exception to the rules, it is possible to allow
    individual users or groups to skip the rules for projects or branches.
    
    Change-Id: I1e11288cd60690754ff4a38499dfcc4385190125

--- src/main/java/com/googlesource/gerrit/plugins/uploadvalidator/ValidatorConfig.java
+++ src/main/java/com/googlesource/gerrit/plugins/uploadvalidator/ValidatorConfig.java
@@ -16,15 +16,21 @@ package com.googlesource.gerrit.plugins.uploadvalidator;
 
 import com.google.gerrit.common.data.RefConfigSection;
 import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gerrit.reviewdb.client.AccountGroup.UUID;
 import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.config.PluginConfig;
 import com.google.gerrit.server.config.PluginConfigFactory;
 import com.google.gerrit.server.project.ProjectCache;
 import com.google.gerrit.server.project.ProjectState;
 import com.google.gerrit.server.project.RefPatternMatcher;
 import com.google.inject.Inject;
+import com.google.inject.Provider;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
 
 public class ValidatorConfig {
   private static final Logger log = LoggerFactory.getLogger(ValidatorConfig.class);
@@ -32,27 +38,36 @@ public class ValidatorConfig {
   private final String pluginName;
   private final ProjectCache projectCache;
   private final PluginConfigFactory pluginCfgFactory;
+  private final Provider<CurrentUser> userProvider;
 
   @Inject
   public ValidatorConfig(@PluginName String pluginName,
       ProjectCache projectCache,
-      PluginConfigFactory pluginCfgFactory) {
+      PluginConfigFactory pluginCfgFactory,
+      Provider<CurrentUser> userProvider) {
     this.pluginName = pluginName;
     this.projectCache = projectCache;
     this.pluginCfgFactory = pluginCfgFactory;
+    this.userProvider = userProvider;
   }
 
-  public boolean isEnabledForRef(Project.NameKey projectName, String refName) {
+  public boolean isEnabledForRef(Project.NameKey projectName, String refName,
+      String validatorOp) {
     ProjectState project = projectCache.get(projectName);
     if (project == null) {
       log.error("Failed to check if " + pluginName + " is enabled for project "
           + projectName.get() + ": Project " + projectName.get() + " not found");
       return false;
     }
-
-    String[] refPatterns =
+    PluginConfig pluginProjectConfig =
         pluginCfgFactory.getFromProjectConfigWithInheritance(project,
-            pluginName).getStringList("branch");
+            pluginName);
+
+    if (canSkipOnRef(pluginProjectConfig, refName, validatorOp)) {
+      return false;
+    }
+
+    String[] refPatterns = pluginProjectConfig.getStringList("branch");
     if (refPatterns.length == 0) {
       return true; // Default behavior: no branch-specific config
     }
@@ -63,6 +78,51 @@ public class ValidatorConfig {
       }
     }
     return false;
+  }
+
+  private boolean canSkipOnRef(PluginConfig pluginProjectConfig,
+      String refName, String validation) {
+    if (!skipValidation(pluginProjectConfig, validation)) {
+      return false;
+    }
+
+    String[] skipBranches = pluginProjectConfig.getStringList("skipBranch");
+    if (skipBranches.length == 0) {
+      return canSkipOnGroup(pluginProjectConfig);
+    }
+
+    for (String refPattern : skipBranches) {
+      if (RefConfigSection.isValid(refPattern) && match(refName, refPattern)) {
+        return canSkipOnGroup(pluginProjectConfig);
+      }
+    }
+
+    return false;
+  }
+
+  private boolean skipValidation(PluginConfig pluginProjectConfig,
+      String validation) {
+    for (String s : pluginProjectConfig.getStringList("skipValidation")) {
+      if (s.equals(validation)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean canSkipOnGroup(PluginConfig pluginProjectConfig) {
+    String[] skipGroups = pluginProjectConfig.getStringList("skipGroup");
+    if (skipGroups.length == 0) {
+      return false;
+    }
+
+    CurrentUser user = userProvider.get();
+    if (!user.isIdentifiedUser()) {
+      return false;
+    }
+
+    return user.asIdentifiedUser().getEffectiveGroups()
+        .containsAnyOf(Arrays.stream(skipGroups).map(UUID::parse)::iterator);
   }
 
   private static boolean match(String refName, String refPattern) {
