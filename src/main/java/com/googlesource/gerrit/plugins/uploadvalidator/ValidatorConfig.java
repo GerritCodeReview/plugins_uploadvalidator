983d3127f2ebb2604ce52700a5aa7d2de1372675
Luca Milanesio <luca.milanesio@gmail.com>
2017-01-19 22:21:21 +0000

    Separate concerns of Plugin and validation config
    Isolate the logic of reading the configuration for a project
    from the enable/disable validation logic.
    This is a preparation work to make the plugin testable in different
    scenarios.
    
    Change-Id: Ife593cddf198eec72767b5eb32a6f1cb66d3a5a4

--- src/main/java/com/googlesource/gerrit/plugins/uploadvalidator/ValidatorConfig.java
+++ src/main/java/com/googlesource/gerrit/plugins/uploadvalidator/ValidatorConfig.java
@@ -15,55 +15,32 @@
 package com.googlesource.gerrit.plugins.uploadvalidator;
 
 import com.google.gerrit.common.data.RefConfigSection;
-import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.reviewdb.client.AccountGroup.UUID;
 import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.config.PluginConfig;
-import com.google.gerrit.server.config.PluginConfigFactory;
-import com.google.gerrit.server.project.ProjectCache;
-import com.google.gerrit.server.project.ProjectState;
 import com.google.gerrit.server.project.RefPatternMatcher;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.Arrays;
 
 public class ValidatorConfig {
-  private static final Logger log = LoggerFactory.getLogger(ValidatorConfig.class);
-
-  private final String pluginName;
-  private final ProjectCache projectCache;
-  private final PluginConfigFactory pluginCfgFactory;
+  private final ConfigFactory pluginCfgFactory;
   private final Provider<CurrentUser> userProvider;
 
   @Inject
-  public ValidatorConfig(@PluginName String pluginName,
-      ProjectCache projectCache,
-      PluginConfigFactory pluginCfgFactory,
+  public ValidatorConfig(ConfigFactory pluginCfgFactory,
       Provider<CurrentUser> userProvider) {
-    this.pluginName = pluginName;
-    this.projectCache = projectCache;
     this.pluginCfgFactory = pluginCfgFactory;
     this.userProvider = userProvider;
   }
 
   public boolean isEnabledForRef(Project.NameKey projectName, String refName,
       String validatorOp) {
-    ProjectState project = projectCache.get(projectName);
-    if (project == null) {
-      log.error("Failed to check if " + pluginName + " is enabled for project "
-          + projectName.get() + ": Project " + projectName.get() + " not found");
-      return false;
-    }
-    PluginConfig pluginProjectConfig =
-        pluginCfgFactory.getFromProjectConfigWithInheritance(project,
-            pluginName);
-
-    if (canSkipOnRef(pluginProjectConfig, refName, validatorOp)) {
+    PluginConfig pluginProjectConfig = pluginCfgFactory.get(projectName);
+    if (pluginProjectConfig == null
+        || canSkipOnRef(pluginProjectConfig, refName, validatorOp)) {
       return false;
     }
 
