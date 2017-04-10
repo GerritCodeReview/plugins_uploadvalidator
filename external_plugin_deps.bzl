535d00a60011c5733f6ebd294b633635f6cf13c3
Stephen Li <stephenli@google.com>
2017-04-10 16:53:05 -0700

    Remove Tika dependency for uploadvalidator
    Tika was originally used over the MimeUtils2 library
    because it was claimed that project.config was detected
    as application/octet-stream. While project.config is in
    fact detected as text/x-ini, other .config files are
    detected as application/octet-stream.
    
    Once change 103290 makes all .config files detected as
    text/x-ini, we can remove the dependency on Tika introduced
    in change 76268.
    
    Change-Id: Ib9a64557b89058fc246a611c3e8a1be74eecf042

--- external_plugin_deps.bzl
+++
@@ -1,8 +0,0 @@
-load("//tools/bzl:maven_jar.bzl", "maven_jar")
-
-def external_plugin_deps():
-  maven_jar(
-      name = 'tika_core',
-      artifact = 'org.apache.tika:tika-core:1.12',
-      sha1 = '5ab95580d22fe1dee79cffbcd98bb509a32da09b',
-  )
--- src/test/java/com/googlesource/gerrit/plugins/uploadvalidator/InvalidLineEndingValidatorTest.java
+++ src/test/java/com/googlesource/gerrit/plugins/uploadvalidator/InvalidLineEndingValidatorTest.java
@@ -53,9 +53,7 @@ public class InvalidLineEndingValidatorTest extends ValidatorTestCase {
   public void testCarriageReturn() throws Exception {
     try (RevWalk rw = new RevWalk(repo)) {
       RevCommit c = makeCommit(rw);
-      InvalidLineEndingValidator validator =
-          new InvalidLineEndingValidator(
-              null, new ContentTypeUtil(PATTERN_CACHE), null, null, null);
+      InvalidLineEndingValidator validator = new InvalidLineEndingValidator(null, null, null, null);
       List<CommitValidationMessage> m =
           validator.performValidation(repo, c, rw, EMPTY_PLUGIN_CONFIG);
       assertThat(TestUtils.transformMessages(m))
