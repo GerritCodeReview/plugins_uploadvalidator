e165b1d6841ceb0b0c4b79d2c889efd7ce6652ae
Stephen Li <stephenli@google.com>
2017-04-10 17:27:14 -0700

    Use jgit RawText's isBinary over MimeTypes
    RawText's isBinary will allow us to detect whether or not
    a file is text-based regardless of the content type, allowing
    us to alleviate some misdetection based on binary types.
    
    Change-Id: I2726af4bf3235ee7a03cc06d88c9877f7081ae4b

--- src/test/java/com/googlesource/gerrit/plugins/uploadvalidator/BlockedKeywordValidatorTest.java
+++ src/test/java/com/googlesource/gerrit/plugins/uploadvalidator/BlockedKeywordValidatorTest.java
@@ -77,8 +77,7 @@ public class BlockedKeywordValidatorTest extends ValidatorTestCase {
     try (RevWalk rw = new RevWalk(repo)) {
       RevCommit c = makeCommit(rw);
       BlockedKeywordValidator validator =
-          new BlockedKeywordValidator(
-              null, new ContentTypeUtil(PATTERN_CACHE), PATTERN_CACHE, null, null, null);
+          new BlockedKeywordValidator(null, PATTERN_CACHE, null, null, null);
       List<CommitValidationMessage> m =
           validator.performValidation(repo, c, rw, getPatterns().values(), EMPTY_PLUGIN_CONFIG);
       Set<String> expected =
