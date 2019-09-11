f58588e98ced3edf8abe4b1abbdfa9ccc3e6f064
Brian Egan <begizi@google.com>
2019-09-11 14:40:05 -0700

    Add copywrite into
    Change-Id: I27ac5fd9497ccec3890e4ba879912f5d764f033b

--- src/main/java/com/googlesource/gerrit/plugins/uploadvalidator/BlockedKeywordConfigLoader.java
+++ src/main/java/com/googlesource/gerrit/plugins/uploadvalidator/BlockedKeywordConfigLoader.java
@@ -1,3 +1,17 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.googlesource.gerrit.plugins.uploadvalidator;
 
 import static com.googlesource.gerrit.plugins.uploadvalidator.BlockedKeywordValidator.KEY_CHECK_BLOCKED_KEYWORD;
@@ -5,6 +19,7 @@ import static com.googlesource.gerrit.plugins.uploadvalidator.BlockedKeywordVali
 import static com.googlesource.gerrit.plugins.uploadvalidator.PatternCacheModule.CACHE_NAME;
 
 import com.google.common.cache.LoadingCache;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.reviewdb.client.Project.NameKey;
