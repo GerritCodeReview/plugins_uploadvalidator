2d44aa88d83dbf4314dc5980ec4bde49559622b4
Patrick Hiesel <hiesel@google.com>
2021-05-19 11:15:11 +0200

    Remove fallback to check entire files
    Now that we have figured out object visibility in Gerrit core, we
    can add a test to check the diff instead of the whole file even
    when changes/patch sets are created on the REST API.
    
    Depends-On: 306930
    Change-Id: I6e1d99f3fbf411a97ec0641e8633886812909864

--- src/main/java/com/googlesource/gerrit/plugins/uploadvalidator/BlockedKeywordValidator.java
+++ src/main/java/com/googlesource/gerrit/plugins/uploadvalidator/BlockedKeywordValidator.java
@@ -179,7 +179,10 @@ public class BlockedKeywordValidator implements CommitValidationListener, Commen
           }
         }
       }
-    } catch (NoSuchProjectException | IOException | ExecutionException e) {
+    } catch (NoSuchProjectException
+        | IOException
+        | ExecutionException
+        | PatchListNotAvailableException e) {
       throw new CommitValidationException("failed to check on blocked keywords", e);
     }
     return Collections.emptyList();
@@ -221,24 +224,14 @@ public class BlockedKeywordValidator implements CommitValidationListener, Commen
       RevWalk revWalk,
       ImmutableCollection<Pattern> blockedKeywordPatterns,
       PluginConfig cfg)
-      throws IOException, ExecutionException {
+      throws IOException, ExecutionException, PatchListNotAvailableException {
     List<CommitValidationMessage> messages = new LinkedList<>();
     checkCommitMessageForBlockedKeywords(blockedKeywordPatterns, messages, c.getFullMessage());
     Map<String, ObjectId> content = CommitUtils.getChangedContent(repo, c, revWalk);
-    Optional<PatchList> patchList;
-    try {
-      patchList =
-          Optional.of(
-              patchListCache.get(
-                  PatchListKey.againstDefaultBase(c, DiffPreferencesInfo.Whitespace.IGNORE_NONE),
-                  project));
-    } catch (PatchListNotAvailableException e) {
-      // TODO(hiesel): Make changes to Gerrit core to always get a diff here. This entails figuring
-      // out Git object visibility between unflushed ObjectInserters and ObjectReaders created from
-      // the repo.
-      logger.atInfo().withCause(e).log("unable to load patch set");
-      patchList = Optional.empty();
-    }
+    PatchList patchList =
+        patchListCache.get(
+            PatchListKey.againstDefaultBase(c, DiffPreferencesInfo.Whitespace.IGNORE_NONE),
+            project);
 
     for (String path : content.keySet()) {
       ObjectLoader ol = revWalk.getObjectReader().open(content.get(path));
@@ -247,12 +240,8 @@ public class BlockedKeywordValidator implements CommitValidationListener, Commen
           continue;
         }
       }
-      if (patchList.isPresent()) {
-        checkLineDiffForBlockedKeywords(
-            patchList.get().get(path).getEdits(), blockedKeywordPatterns, messages, path, ol);
-      } else {
-        checkEntireFileForBlockedKeywords(blockedKeywordPatterns, messages, path, ol);
-      }
+      checkLineDiffForBlockedKeywords(
+          patchList.get(path).getEdits(), blockedKeywordPatterns, messages, path, ol);
     }
     return messages;
   }
@@ -300,22 +289,6 @@ public class BlockedKeywordValidator implements CommitValidationListener, Commen
     for (Edit edit : edits) {
       for (int i = edit.getBeginB(); i < edit.getEndB(); i++) {
         checkLineForBlockedKeywords(blockedKeywordPatterns, messages, path, i + 1, lines.get(i));
-      }
-    }
-  }
-
-  private static void checkEntireFileForBlockedKeywords(
-      ImmutableCollection<Pattern> blockedKeywordPatterns,
-      List<CommitValidationMessage> messages,
-      String path,
-      ObjectLoader ol)
-      throws IOException {
-    try (BufferedReader br =
-        new BufferedReader(new InputStreamReader(ol.openStream(), StandardCharsets.UTF_8))) {
-      int line = 1;
-      for (String l = br.readLine(); l != null; l = br.readLine()) {
-        checkLineForBlockedKeywords(blockedKeywordPatterns, messages, path, line, l);
-        line++;
       }
     }
   }
