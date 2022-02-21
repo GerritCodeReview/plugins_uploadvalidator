785d68b8bf3ed546e826ba60640b98734d73495c
Wendy Wang <wendy.wang10@sap.com>
2022-02-21 16:00:15 +0100

    Project Config Validation for Upload Validator Plugin
    Added new listener to validate changes to project.config. Email
    restrictions will be checked for legal regex, illegal regex will cause
    an error to be thrown and the commit to be rejected.
    
    Validation functionality will be extended to other fields as well.
    Email restrictions are currently not checked for legal regex when the
    upload validator settings are changed in the UI.
    
    Change-Id: Ied7fbe5b2d52ac40b04ac7f9cd5043e6064d855d

---
+++ src/test/java/com/googlesource/gerrit/plugins/uploadvalidator/ChangeConfigTest.java
@@ -0,0 +1,75 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.uploadvalidator;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.eclipse.jgit.lib.Config;
+import org.junit.Test;
+
+public class ChangeConfigTest {
+  private String projectName = "testProject";
+  private String pluginName = "uploadvalidator";
+  private static final String illegalRegex = "*";
+  private static final String legalRegex = ".*";
+  private static final String legalPathLength = "100";
+  private static final String illegalPathLength = "10xi";
+  private PluginConfigValidator configValidator = new PluginConfigValidator(pluginName);
+  private Config cfg = new Config();
+
+  @Test
+  public void hasLegalAuthorEmailPattern() throws Exception {
+    String configKey = ChangeEmailValidator.KEY_ALLOWED_AUTHOR_EMAIL_PATTERN;
+    cfg.setString("plugins", pluginName, configKey, legalRegex);
+
+    assertThat((configValidator.validateRegex(projectName, cfg, configKey)).isEmpty());
+  }
+
+  @Test
+  public void hasLegalCommitterEmailPattern() throws Exception {
+    String configKey = ChangeEmailValidator.KEY_ALLOWED_COMMITTER_EMAIL_PATTERN;
+    cfg.setString("plugins", pluginName, configKey, legalRegex);
+    assertThat((configValidator.validateRegex(projectName, cfg, configKey)).isEmpty());
+  }
+
+  @Test
+  public void hasIllegalAuthorEmailPattern() throws Exception {
+    String configKey = ChangeEmailValidator.KEY_ALLOWED_AUTHOR_EMAIL_PATTERN;
+    cfg.setString("plugins", pluginName, configKey, illegalRegex);
+    assertThat(!(configValidator.validateRegex(projectName, cfg, configKey)).isEmpty());
+  }
+
+  @Test
+  public void hasIllegalCommitterEmailPattern() throws Exception {
+    String configKey = ChangeEmailValidator.KEY_ALLOWED_COMMITTER_EMAIL_PATTERN;
+    cfg.setString("plugins", pluginName, configKey, illegalRegex);
+    assertThat(!(configValidator.validateRegex(projectName, cfg, configKey)).isEmpty());
+  }
+
+  @Test
+  public void hasLegalMaxPathLength() throws Exception {
+    String configKey = MaxPathLengthValidator.KEY_MAX_PATH_LENGTH;
+    cfg.setString("plugins", pluginName, configKey, legalPathLength);
+    assertThat((configValidator.validateInteger(projectName, cfg, configKey)).isEmpty());
+  }
+
+  @Test
+  public void hasIllegalMaxPathLength() throws Exception {
+    String configKey = MaxPathLengthValidator.KEY_MAX_PATH_LENGTH;
+    cfg.setString("plugins", pluginName, configKey, illegalPathLength);
+
+    assertThat(!(configValidator.validateInteger(projectName, cfg, configKey)).isEmpty());
+  }
+}
