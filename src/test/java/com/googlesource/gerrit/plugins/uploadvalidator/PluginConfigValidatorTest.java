75c4ea1b6daf0e73b0f9faa7ec323a4f22a93063
Wendy Wang <wendy.wang10@sap.com>
2022-02-21 16:00:15 +0100

    Project Config Validation for Upload Validator Plugin
    Added new listener to validate changes to project.config. Email
    restrictions will be checked for legal regex, illegal regex will cause
    an error to be thrown and the commit to be rejected.
    
    Validation functionality will be extended to other fields as well.
    Email restrictions are currently not checked for legal regex when the
    upload validator settings are changed in the UI.
    
    Change-Id: Ied7fbe5b2d52ac40b04ac7f9cd5043e6064d855d

---
+++ src/test/java/com/googlesource/gerrit/plugins/uploadvalidator/PluginConfigValidatorTest.java
@@ -0,0 +1,73 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.uploadvalidator;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.googlesource.gerrit.plugins.uploadvalidator.ChangeEmailValidator.KEY_ALLOWED_AUTHOR_EMAIL_PATTERN;
+import static com.googlesource.gerrit.plugins.uploadvalidator.ChangeEmailValidator.KEY_ALLOWED_COMMITTER_EMAIL_PATTERN;
+import static com.google.gerrit.server.project.ProjectConfig.PROJECT_CONFIG;
+
+import com.google.common.collect.ImmutableList;
+import com.google.gerrit.server.git.validators.CommitValidationMessage;
+import org.eclipse.jgit.lib.Config;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PluginConfigValidatorTest {
+  private static final String PLUGIN_NAME = "uploadvalidator";
+  private static final String ILLEGAL_REGEX = "*";
+  private static final String LEGAL_REGEX = ".*";
+
+  private PluginConfigValidator configValidator;
+  private Config cfg;
+
+  @Before
+  public void setUp() throws Exception {
+    configValidator = new PluginConfigValidator(PLUGIN_NAME);
+    cfg = new Config();
+  }
+
+  @Test
+  public void hasLegalAuthorEmailPattern_noMessages() throws Exception {
+    cfg.setString("plugin", PLUGIN_NAME, KEY_ALLOWED_AUTHOR_EMAIL_PATTERN, LEGAL_REGEX);
+    ImmutableList<CommitValidationMessage> messages =
+        configValidator.validateRegex(PROJECT_CONFIG, cfg, KEY_ALLOWED_AUTHOR_EMAIL_PATTERN);
+    assertThat(messages).isEmpty();
+  }
+
+  @Test
+  public void hasIllegalAuthorEmailPattern_messages() throws Exception {
+    cfg.setString("plugin", PLUGIN_NAME, KEY_ALLOWED_AUTHOR_EMAIL_PATTERN, ILLEGAL_REGEX);
+    ImmutableList<CommitValidationMessage> messages =
+        configValidator.validateRegex(PROJECT_CONFIG, cfg, KEY_ALLOWED_AUTHOR_EMAIL_PATTERN);
+    assertThat(messages).isNotEmpty();
+  }
+
+  @Test
+  public void hasLegalCommitterEmailPattern_noMessages() throws Exception {
+    cfg.setString("plugin", PLUGIN_NAME, KEY_ALLOWED_COMMITTER_EMAIL_PATTERN, LEGAL_REGEX);
+    ImmutableList<CommitValidationMessage> messages =
+        configValidator.validateRegex(PROJECT_CONFIG, cfg, KEY_ALLOWED_COMMITTER_EMAIL_PATTERN);
+    assertThat(messages).isEmpty();
+  }
+
+  @Test
+  public void hasIllegalCommitterEmailPattern_messages() throws Exception {
+    cfg.setString("plugin", PLUGIN_NAME, KEY_ALLOWED_COMMITTER_EMAIL_PATTERN, ILLEGAL_REGEX);
+    ImmutableList<CommitValidationMessage> messages =
+        configValidator.validateRegex(PROJECT_CONFIG, cfg, KEY_ALLOWED_COMMITTER_EMAIL_PATTERN);
+    assertThat(messages).isNotEmpty();
+  }
+}
