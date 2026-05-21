9c79ccf918e230ebfcefdfffe10e20696ec7c46c
Luca Milanesio <luca.milanesio@gmail.com>
2026-05-21 18:39:22 +0100

    Revert "Disable the content-based mime detection tests"
    This reverts commit 8e6e14061c74cf507629676ac1f4c67da1c78371.
    
    Change-Id: Ic49cd092eb39f1d39a66e62f67b3829cf444e425

--- src/test/java/com/googlesource/gerrit/plugins/uploadvalidator/MimeTypeDetectionTest.java
+++ src/test/java/com/googlesource/gerrit/plugins/uploadvalidator/MimeTypeDetectionTest.java
@@ -86,12 +86,8 @@ public class MimeTypeDetectionTest extends ValidatorTestCase {
     content = "Hello,World";
     files.add(new FileContent("foo.csv", content.getBytes(StandardCharsets.UTF_8), "text/csv"));
 
-    // See https://issues.gerritcodereview.com/issues/495458797 and the inaccurate content-based
-    // type
-    // detection of the com.github.albfernandez:juniversalchardet:2.0.0 included in Gerrit v3.12.
-    // TODO: To be enabled again in the merge-up to stable-3.13 onwards
-    //    content = "hello=world";
-    //    files.add(new FileContent("foo", content.getBytes(StandardCharsets.UTF_8), "text/plain"));
+    content = "hello=world";
+    files.add(new FileContent("foo", content.getBytes(StandardCharsets.UTF_8), "text/plain"));
 
     files.add(new FileContent("foo.pdf", TEST_PDF, "application/pdf"));
     return files;
